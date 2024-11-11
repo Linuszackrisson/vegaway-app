d4d08167eceab9b5421d1094851d30ae9e8ce33d
Linuszackrisson <linus.gz@hotmail.com>
2024-11-11 13:53:14 +0100

    Addes pages and folder structure aswell as routing between these

--- vegaway-app/src/App.tsx
+++ vegaway-app/src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import About from './pages/AboutPage/About';
+import MenuPage from './pages/MenuPage/MenuPage';
+import DashboardPage from './pages/DashboardPage/DashboardPage';
+import CartPage from './pages/CartPage/CartPage';
 
 function App() {
   return (
-    <div>
-      <h1>Hello Vegaway</h1>
+    <Router>
+   <div>
+      <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/menu' element={<MenuPage />} />
+      <Route path='/dashboard' element={<DashboardPage />} />
+      <Route path='/cart' element={<CartPage />} />
+      </Routes>
     </div>
-  )
+    </Router>
+  );
 }
 
 export default App
---
+++ vegaway-app/src/pages/MenuPage/MenuPage.tsx
@@ -0,0 +1,11 @@
+import './MenuPage.css'; 
+
+function MenuPage() {
+  return (
+    <div>
+      <h1>Menu Page</h1>
+    </div>
+  )
+}
+
+export default MenuPage
