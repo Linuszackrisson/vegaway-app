b0a203b1101d39c2f79177d6bf0eaa3890ae0fb3
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-11-13 02:42:46 +0100

    using correct auth header

--- vegaway-sls/middlewares/validateKey.js
+++ vegaway-sls/middlewares/validateKey.js
@@ -1,16 +1,24 @@
 require("dotenv").config(); // Load environment variables from .env
 
+// Custom Error class that includes statusCode
+class ApiKeyError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
 module.exports = () => ({
   before: (handler) => {
-    const { key } = handler.event.headers?.Authorization;
+    const key = handler.event.headers?.authorization;
 
     if (!key) {
-      throw new Error("API key missing");
+      throw new ApiKeyError("API key missing", 400); // Bad Request
     }
 
     // Validate the provided key against the API_KEY in .env
     if (key !== process.env.API_KEY) {
-      throw new Error("Invalid API key");
+      throw new ApiKeyError("Invalid API key", 401); // Unauthorized
     }
   },
 });
