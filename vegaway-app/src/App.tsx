499876c260bec26ebb57b2f408e4ca0f701bc4c6
jacobcollen <jcbcollen@icloud.com>
2024-11-14 18:19:06 +0100

    ProductCard, ProductSlider, ProductPage, MenuPage + some global css

--- vegaway-app/src/App.tsx
+++ vegaway-app/src/App.tsx
@@ -1,30 +1,35 @@
+// src/App.tsx
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import About from "./pages/AboutPage/About";
 import MenuPage from "./pages/MenuPage/MenuPage";
+import ProductPage from "./pages/productPage/ProductPage";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
 import CartPage from "./pages/CartPage/CartPage";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/menu" element={<MenuPage />} />
+					<Route path="/product/:id" element={<ProductPage />} />
+					<Route path="/dashboard" element={<DashboardPage />} />
+					<Route path="/cart" element={<CartPage />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
 
 /**
  * Författare Linus
- * App.jsx boiler plate, importer och routes setup.
+  App.tsx boilerplate med importer och routes setup.
+  Uppdaterad
+  av Jacob: Lagt till routing för ProductPage 
  */
