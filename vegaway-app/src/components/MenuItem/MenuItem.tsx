15012963f6bada7431b230f77dc65c6aa33945dc
Linuszackrisson <linus.gz@hotmail.com>
2024-11-14 13:02:01 +0100

    Mobile menu

--- vegaway-app/src/components/MenuItem/MenuItem.tsx
+++ vegaway-app/src/components/MenuItem/MenuItem.tsx
@@ -1,6 +1,7 @@
 // vegaway-app/src/components/MenuItem/MenuItem.tsx
 import React, { useState } from "react";
 import { MenuItem as MenuItemType } from "../../api/menuApi";
+import './MenuItem.css'
 
 interface MenuItemProps {
   item: MenuItemType;
@@ -17,22 +18,29 @@ const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
     <li className="menu-item" key={item.menuId}>
       {isExpanded ? (
         <div className="menu-item__expanded">
-          <img className="menu-item__image--large" src="https://placehold.co/280x230" alt={item.name} />
-          <p className="menu-item__title">{item.name}</p>
-          <p className="menu-item__description">{item.description}</p>
-          <p className="menu-item__price">Price: ${item.price.toFixed(2)}</p>
+          <div className="menu-item__image-container">
+            <img className="menu-item__image--large" src="https://placehold.co/350x300" alt={item.name} />
+          </div>
+          <div className="menu-item__info">
+            <p className="menu-item__title">{item.name}</p>
+            <p className="menu-item__price">${item.price.toFixed(2)}</p>
+            </div>
+            <p className="menu-item__desc">{item.description}</p>
+
+          
           <button className="menu-item__close-button" onClick={toggleExpand}>Close</button>
         </div>
       ) : (
         <div className="menu-item__collapsed">
           <div className="menu-item__image-container">
-            <img className="menu-item__image--small" src="https://placehold.co/140x115" alt={item.name} />
-            <button className="menu-item__add-button">Add to Cart</button>
+            <img className="menu-item__image--small" src="https://placehold.co/200x115" alt={item.name} />
+            <button className="menu-item__add-button">+</button>
           </div>
           <div className="menu-item__info">
             <div className="menu-item__title-price">
               <p className="menu-item__title">{item.name}</p>
-              <p className="menu-item__price">Price: ${item.price.toFixed(2)}</p>
+              <p className="menu-item__price">${item.price.toFixed(2)}</p>
+
             </div>
             <button className="menu-item__info-button" onClick={toggleExpand}>info &gt;</button> {/* Info-knapp längst till vänster */}
           </div>
