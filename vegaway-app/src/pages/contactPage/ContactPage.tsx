9c321e3823b4a13a7637ff830bb0749369cefda4
Linuszackrisson <linus.gz@hotmail.com>
2024-11-18 08:30:44 +0100

    Adjustments

--- vegaway-app/src/pages/contactPage/ContactPage.tsx
+++ vegaway-app/src/pages/contactPage/ContactPage.tsx
@@ -1,4 +1,6 @@
 import './ContactPage.css'; 
+import navigationIcon from '../../assets/navigation.png';
+import arrowIcon from '../../assets/arrowright.png'
 
 function ContactPage() {
   return (
@@ -7,7 +9,11 @@ function ContactPage() {
       <p className='contact-page__description'>We usually reply within a few hours .. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has ... </p>
       <form id="kontaktForm" className='contact-page__form'>
         <textarea id="meddelande" name="meddelande" className='contact-page__textarea' placeholder="Write your message ..."></textarea>
-        <button type="submit" className='contact-page__button'>Send</button>
+        <button type="submit" className='contact-page__button'>
+          <img src={navigationIcon} alt="Navigation" className='button-icon' />
+          <p className='button_p'>Send</p>
+          <img src={arrowIcon} alt="Arrow Right" className='button-icon' />
+        </button>
       </form>
     </div>
   )
