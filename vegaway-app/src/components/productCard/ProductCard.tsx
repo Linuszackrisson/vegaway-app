48a857e86997e90a80f1f56728e7a57225525ad7
jacobcollen <jcbcollen@icloud.com>
2024-11-20 02:43:48 +0100

    grid, slider och media quaries

--- vegaway-app/src/components/productCard/ProductCard.tsx
+++ vegaway-app/src/components/productCard/ProductCard.tsx
@@ -38,7 +38,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ item }) => {
       </div> 
       <div className="product-card__info"> 
         <div className="product-card__title-price"> 
-          <p className="product-card__title">{item.name}</p> 
+          <h3 className="product-card__title">{item.name}</h3> 
           <p className="product-card__price">${item.price.toFixed(2)}</p> 
         </div> 
         <button 
