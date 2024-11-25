508267e6c46e7cfd3293247b30557b7f966fb1b4
Isak <yhfe23.skolis@folkuniversitetet.nu>
2024-11-25 17:09:44 +0100

    Made handle click function async

--- vegaway-app/src/api/utils/testing/RefreshStatusBtn.tsx
+++ vegaway-app/src/api/utils/testing/RefreshStatusBtn.tsx
@@ -1,10 +1,11 @@
 import { refreshStatus } from "../../refreshStatus";
 
 const RefreshStatusBtn = () => {
-  const orderId = "order-1732527529607";
+  const orderId = "order-1732408284436";
 
-  const handleClick = () => {
-    refreshStatus(orderId);
+  const handleClick = async () => {
+    const response = await refreshStatus(orderId);
+    console.log(response);
   };
   return <button onClick={handleClick}>Refresh order status</button>;
 };
