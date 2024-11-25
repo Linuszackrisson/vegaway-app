5b8997351e32c1c340477dfeca2bae961a51846c
Linuszackrisson <linus.gz@hotmail.com>
2024-11-25 18:09:33 +0100

    more positioning

--- vegaway-app/src/pages/HomePage/HomePage.tsx
+++ vegaway-app/src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ import ProductSlider from '../../components/productSlider/ProductSlider';
 import likeIcon from '../../assets/like.svg';
 import instagramIcon from '../../assets/instagram.svg';
 import newIcon from '../../assets/new.svg';
-import chevronIcon from '../../assets/chevron-right.svg';
+import chevronIcon from '../../assets/chevron-right2.svg';
 
 function HomePage() {
   const [featuredProducts, setFeaturedProducts] = useState<MenuItem[]>([]);
@@ -21,13 +21,13 @@ function HomePage() {
 
   return (
     <>
-      
+      <div className="wrapper">
       <div className="button-container">
         <img className='like-icon' src={likeIcon} alt="like"/>
-        <button className='button__first ig-white'>We're on Instagram</button>
+        <button className='button__first ig-white'> <p>We're on Instagram</p></button>
         <button className='button__first ig-yellow'>
           <img src={instagramIcon} alt="Instagram"/>
-          Check us out
+          <p>Check us out</p>
           <img src={chevronIcon} alt="Chevron Right"/>
         </button>
       </div>
@@ -35,7 +35,7 @@ function HomePage() {
       {featuredProducts.length > 0 && (
         <ProductSlider title="New Releases" items={featuredProducts} />
       )}
-      
+      </div>
     </>
   );
 }
