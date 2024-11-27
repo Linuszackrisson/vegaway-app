13babfbff6d9b3cab35b04c3fc911c69999d3fe2
Isak <yhfe23.skolis@folkuniversitetet.nu>
2024-11-27 15:04:49 +0100

    Created api request file for updating an order

---
+++ vegaway-app/src/api/updateOrder.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const invokeUrl = import.meta.env.VITE_INVOKE_URL;
+const API_KEY = "MY_API_KEY";
+import { useCurrentOrderStore } from "../store/useCurrentOrderStore";
+
+export async function updateOrder() {
+  try {
+    const accessToken = localStorage.getItem("access_token");
+    const idToken = localStorage.getItem("id_token");
+
+    if (!idToken || !accessToken) {
+      throw new Error("Access or id token not found");
+    }
+
+    const { order } = useCurrentOrderStore.getState();
+
+    const response = await axios.put(`${invokeUrl}/orders`, order, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: API_KEY,
+        "x-cognito-auth": `Bearer ${accessToken}`,
+        "x-cognito-id": `Bearer ${idToken}`,
+      },
+    });
+
+    return response.data.data;
+  } catch (error) {
+    console.error("Error creating order:", error);
+    throw new Error(`Failed to create order: ${error}`);
+  }
+}
+
+/*
+ * Författare: Isak
+ *
+ * Api request för att uppdatera en order
+ * Skickar order från useCurrentOrderStore till backend som hanterar uppdateringen
+ */
