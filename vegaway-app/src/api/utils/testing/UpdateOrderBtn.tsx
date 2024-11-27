c2a81f77a012775d5e2223163b885c0ec1d8a0a0
Isak <yhfe23.skolis@folkuniversitetet.nu>
2024-11-27 16:31:21 +0100

    Bugfix on test button

--- vegaway-app/src/api/utils/testing/UpdateOrderBtn.tsx
+++ vegaway-app/src/api/utils/testing/UpdateOrderBtn.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 import { updateOrder } from "../../updateOrder";
 
-const UpdateOrderBtn = async () => {
-  const handleClick = await updateOrder();
+const UpdateOrderBtn = () => {
+  const handleClick = async () => {
+    const response = await updateOrder();
+    console.log("Update order response:", response);
+  };
   return <button onClick={handleClick}>Update your order!</button>;
 };
 
