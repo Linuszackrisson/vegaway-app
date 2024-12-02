7ed91612d7524e62dc2b74ad9cce1aff6b2c9761
jacobcollen <jcbcollen@icloud.com>
2024-12-02 13:51:42 +0100

    global knapptext-size + anpassat cartbutton styling

--- vegaway-app/src/components/CartButton/CartButton.tsx
+++ vegaway-app/src/components/CartButton/CartButton.tsx
@@ -12,75 +12,73 @@ import { createOrder } from "../../api/placeOrder";
 import { useCurrentOrderStore } from "../../store/useCurrentOrderStore";
 
 const CartButton: React.FC = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const currentOrder = useCurrentOrderStore((state) => state.order);
+	const location = useLocation();
+	const navigate = useNavigate();
+	const currentOrder = useCurrentOrderStore(state => state.order);
 
-  const hideCartButtonPaths = ["/dashboard", "/pending-orders"];
+	const hideCartButtonPaths = ['/dashboard', '/pending-orders'];
 
-  const shouldHideCartButton = hideCartButtonPaths.some((path) =>
-    location.pathname.startsWith(path)
-  );
+	const shouldHideCartButton = hideCartButtonPaths.some(path => location.pathname.startsWith(path));
 
-  if (shouldHideCartButton) {
-    return null;
-  }
+	if (shouldHideCartButton) {
+		return null;
+	}
 
-  const isCartPage = location.pathname === "/cart";
-  const isOrderConfirmationPage = location.pathname === "/order-confirmation";
+	const isCartPage = location.pathname === '/cart';
+	const isOrderConfirmationPage = location.pathname === '/order-confirmation';
 
-  const handleClick = async () => {
-    if (isCartPage) {
-      await createOrder();
-      navigate("/order-confirmation");
-    } else if (isOrderConfirmationPage) {
-      if (currentOrder?.isConfirmed === "true") {
-        navigate("/menu");
-      } else if (!currentOrder) {
-        navigate("/order-history");
-      } else {
-        navigate("/order-confirmation?edit=true");
-      }
-    } else {
-      navigate("/cart");
-    }
-  };
+	const handleClick = async () => {
+		if (isCartPage) {
+			await createOrder();
+			navigate('/order-confirmation');
+		} else if (isOrderConfirmationPage) {
+			if (currentOrder?.isConfirmed === 'true') {
+				navigate('/menu');
+			} else if (!currentOrder) {
+				navigate('/order-history');
+			} else {
+				navigate('/order-confirmation?edit=true');
+			}
+		} else {
+			navigate('/cart');
+		}
+	};
 
-  let buttonIcon;
-  let buttonText;
+	let buttonIcon;
+	let buttonText;
 
-  if (isCartPage) {
-    buttonIcon = <CreditCard />;
-    buttonText = "Complete Order";
-  } else if (isOrderConfirmationPage) {
-    if (currentOrder === null) {
-      // If the currentOrder is null, show "Go to order history" and no icon
-      buttonIcon = null; // You can also set this to "" if preferred
-      buttonText = "Go to order history";
-    } else {
-      // Otherwise, show the usual order confirmation buttons
-      buttonIcon =
-        currentOrder.isConfirmed === "true" ? <ShoppingBag /> : <Edit3 />;
-      buttonText =
-        currentOrder.isConfirmed === "true"
-          ? "Order Something More"
-          : "Edit Your Order";
-    }
-  } else {
-    buttonIcon = <ShoppingCart />;
-    buttonText = "See Your Cart";
-  }
+	if (isCartPage) {
+    buttonIcon = <CreditCard className="button__icon" strokeWidth={1.5} />;
+		buttonText = 'Complete Order';
+	} else if (isOrderConfirmationPage) {
+		if (currentOrder === null) {
+			// If the currentOrder is null, show "Go to order history" and no icon
+			buttonIcon = null; // You can also set this to "" if preferred
+			buttonText = 'Go to order history';
+		} else {
+			// Otherwise, show the usual order confirmation buttons
+			buttonIcon =
+				currentOrder.isConfirmed === 'true' ? (
+					<ShoppingBag className="button__icon" strokeWidth={1.5} />
+				) : (
+					<Edit3 className="button__icon" strokeWidth={1.5} />
+				);
+			buttonText = currentOrder.isConfirmed === 'true' ? 'Order Something More' : 'Edit Your Order';
+		}
+	} else {
+    buttonIcon = <ShoppingCart className="button__icon" strokeWidth={1.5} />;
+		buttonText = 'See Your Cart';
+	}
 
-  return (
-    <button className="cart-button" onClick={handleClick}>
-      <div className="cart-button-content">
-        {buttonIcon}
-        <span>{buttonText}</span>
-        <ChevronRight />
-      </div>
-    </button>
-  );
+	return (
+		<button className="button button--first cart-button" onClick={handleClick}>
+			{buttonIcon}
+			<span className="button__text">{buttonText}</span>
+			<ChevronRight className="button__icon" />
+		</button>
+	);
 };
+
 
 export default CartButton;
 
