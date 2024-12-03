4544d44e76985350ee845c2936470e8948ad4287
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-12-03 21:44:50 +0100

    Trigger feedback component if JOI validation fails

--- vegaway-app/src/pages/pendingOrdersPage/PendingOrders.tsx
+++ vegaway-app/src/pages/pendingOrdersPage/PendingOrders.tsx
@@ -5,12 +5,14 @@ import "./PendingOrders.css";
 import { confirmOrder } from "../../api/confirmOrderStaff";
 import { OrderIdAndNote } from "../../api/confirmOrderStaff";
 import { validateNote } from "../../utils/joi";
+import { useFeedbackStore } from "../../store/useFeedbackStore";
 
 const PendingOrders: React.FC = () => {
   const [pendingOrders, setPendingOrders] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const [notes, setNotes] = useState<Record<string, string>>({});
+  const { setMessage, setVisibility } = useFeedbackStore.getState();
 
   useEffect(() => {
     const getPendingOrders = async () => {
@@ -19,6 +21,7 @@ const PendingOrders: React.FC = () => {
         setPendingOrders(result.orders);
       } catch (err) {
         setError("Kunde inte hämta ordrar.");
+        console.log(error);
       } finally {
         setLoading(false);
       }
@@ -40,6 +43,11 @@ const PendingOrders: React.FC = () => {
     const validationError = validateNote(note);
     if (validationError) {
       setError(validationError);
+
+      // Trigger feedback component
+      setMessage(validationError);
+      setVisibility(true);
+
       return; // Return early to avoid sending request if validation fails
     }
 
@@ -60,10 +68,6 @@ const PendingOrders: React.FC = () => {
 
   if (loading) {
     return <p>Laddar ordrar...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
   }
 
   const handleNoteChange = (orderId: string, note: string) => {
