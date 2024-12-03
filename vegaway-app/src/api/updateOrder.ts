977faf091fe6137c1c27b74dfa21ce4db43c3c92
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-12-03 21:49:54 +0100

    Trigger feedback component if user is not logged in

--- vegaway-app/src/api/updateOrder.ts
+++ vegaway-app/src/api/updateOrder.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useFeedbackStore } from "../store/useFeedbackStore";
 
 const invokeUrl = import.meta.env.VITE_INVOKE_URL;
 const API_KEY = "MY_API_KEY";
@@ -10,6 +11,12 @@ export async function updateOrder() {
     const idToken = localStorage.getItem("id_token");
 
     if (!idToken || !accessToken) {
+      // Get the setter functions from Zustand store
+      const { setMessage, setVisibility } = useFeedbackStore.getState();
+
+      // Set the feedback message and make the overlay visible
+      setMessage("Please login to update order");
+      setVisibility(true);
       throw new Error("Access or id token not found");
     }
 
