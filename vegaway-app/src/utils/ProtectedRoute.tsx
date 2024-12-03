9e33e6f32960331ed4443c1988a4cfbc3493cbf8
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-12-03 18:46:51 +0100

    exported interface

--- vegaway-app/src/utils/ProtectedRoute.tsx
+++ vegaway-app/src/utils/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
-interface DecodedTokenGroups {
+export interface DecodedTokenGroups {
   "cognito:groups": string[]; // Cognito groups claim
 }
 
