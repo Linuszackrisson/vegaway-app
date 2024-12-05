d251175dc9570d1f4dfb43fbcc3840653e614f2b
jacobcollen <jcbcollen@icloud.com>
2024-12-05 12:07:33 +0100

    dashboard layout done

--- vegaway-app/src/pages/dashboardPage/DashboardPage.tsx
+++ vegaway-app/src/pages/dashboardPage/DashboardPage.tsx
@@ -6,34 +6,20 @@ import "./dashboardPage.css";
 const DashboardPage: React.FC = () => {
 	return (
 		<div className="dashboard staff__wrapper">
-			<h1 className="dashboard__heading">Staff</h1>
-			<div className="dashboard__buttons">
-				<Link to="/pending-orders" className="dashboard__button button__second">
+			<h1 className="dashboard__heading px-1">Staff</h1>
+			<div className="dashboard__buttons px-1">
+				<Link to="/pending-orders" className="dashboard__button button--second">
 					<div className="dashboard__icon-area dashboard__icon-area--pending">
 						<Clock />
 					</div>
 					<div className="dashboard__text">Pending Orders</div>
 				</Link>
 
-				<Link to="/active-orders" className="dashboard__button button__second">
+				<Link to="/active-orders" className="dashboard__button button--second">
 					<div className="dashboard__icon-area dashboard__icon-area--active">
 						<Activity />
 					</div>
 					<div className="dashboard__text">Active Orders</div>
-				</Link>
-
-				<Link to="/inventory" className="dashboard__button button__second">
-					<div className="dashboard__icon-area dashboard__icon-area--inventory">
-						<Box />
-					</div>
-					<div className="dashboard__text">Inventory</div>
-				</Link>
-
-				<Link to="/edit-menu" className="dashboard__button button__second">
-					<div className="dashboard__icon-area dashboard__icon-area--edit">
-						<Edit />
-					</div>
-					<div className="dashboard__text">Edit Menu</div>
 				</Link>
 			</div>
 		</div>
