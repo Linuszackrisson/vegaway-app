37b2444e1673e58c4a3f5ad3dd945afae5b7aa98
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2024-12-06 15:12:00 +0100

    Updated file comments

--- vegaway-app/src/components/orderHistory/OrderHistory.tsx
+++ vegaway-app/src/components/orderHistory/OrderHistory.tsx
@@ -14,7 +14,7 @@ const OrderHistory: React.FC<OrderHistoryProps> = ({ order }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const setOrder = useCurrentOrderStore((state) => state.setOrder);
-  const navigate = useNavigate(); // Initialize navigate
+  const navigate = useNavigate();
 
   // Map isConfirmed to status
   const status = order.isConfirmed === "true" ? "Confirmed" : "Active";
@@ -105,3 +105,15 @@ const OrderHistory: React.FC<OrderHistoryProps> = ({ order }) => {
 };
 
 export default OrderHistory;
+
+/* 
+Författare: Jacob
+
+Skapat boilerplate och preppat inför funktionalitet
+*/
+
+/* 
+Uppdatering: Isak
+
+Implementerar handle click för att sätta current order baserat på klickad order, samt navigering till order-confirmation som renderar content baserat på current order state.
+*/
